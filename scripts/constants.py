2cbb65609bdd51a8a681f51cfa6b71c700bb66f4
Hiroshi Ohta <Hiroshi@MacBookPro.local>
2019-09-20 02:36:37 +0900

    Add: 定数ファイルを追加

---
+++ scripts/constants.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+KEY_STORE = str(Path.home()) + "/.secret/keyfile"
+
+ENC_API_KEY = "NpOFq5BCfhPYcvT5XI3tOmxzDl5zmVjtD+V0yMI="
+NTA_API_URL = "https://api.houjin-bangou.nta.go.jp/4/num"
